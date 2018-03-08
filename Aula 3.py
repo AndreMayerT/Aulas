11e7cad674bdc5d1d2dfb74237f6ddd9a249391e
AndreMayerT <36961075+AndreMayerT@users.noreply.github.com>
2018-03-08 12:39:37 -0300

    Add files via upload

---
+++ Aula 3.py
@@ -0,0 +1,22 @@
+"""
+name = input("What's your name?")
+print("Nice to meet you!" + name + "!")
+age = input("Your age?")
+print("So,you are already" + age + "years old," + name + "!")
+
+
+valor = input("Entre com um caractere")
+print("O caractere lido foi" + valor + "\n")
+
+"""
+
+"""
+IntValue = int(input("Entre com um numero inteiro"))
+print("O numero lido foi " , IntValue , "\n")
+"""
+
+
+FNumber = float(input("Entre com um numero real: "))
+print("O numero lido foi ", FNumber, "\n")
+
+
