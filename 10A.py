8a5e830fc4a957fa228619b6550d972269dbb67b
AndreMayerT <36961075+AndreMayerT@users.noreply.github.com>
2018-05-11 15:36:34 -0300

    Update 10A.py

--- 10A.py
+++ 10A.py
@@ -163,6 +163,7 @@ print("Second higher number: ",sec_maior)
 
 #12) Desenvolva uma App em Python que leia um número (NUM) e então imprima
 #os múltiplos de 3 e 5, ao mesmo tempo, no intervalo fechado de 1 a NUM.
+"""
 n = int(input("Type a number: "))
 cont = 1
 a = 0
@@ -173,5 +174,96 @@ while cont < n:
     b = n + 5
     cont += 1
 print(a, b)
+"""
 
 
+#13) Desenvolva uma App em Python que receba 15 números e imprima quantos
+#números maiores que 30 foram digitados.
+
+"""
+n = int(input("Digite um número: "))
+cont = 1
+num = 0
+
+while cont <= 15:
+    n = int(input("Digite um número: "))
+    if n > 30:
+        num += 1
+    cont += 1
+print(num)
+"""
+
+
+
+#14) Desenvolva uma App em Python que leia 20 números e imprima a soma dos
+#positivos e o total de números negativos.
+
+"""
+n = 0
+cont = 1
+neg = 0
+soma = 0
+
+while cont <= 5:
+    n = int(input("Digite um número: "))
+    if n < 0:
+        neg += 1
+    if n >= 0:
+        soma += n
+    cont += 1
+print("Soma dos positivos:", soma,"\nTotal de números negativos:", neg)
+"""
+
+
+
+#15) Desenvolva uma App em Python que determine todos os divisores de um
+#dado número N.
+
+"""
+n = int(input("Digite um número: "))
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i)
+"""
+
+
+
+#16) Seja a seguinte série: 1, 4, 9, 16, 25, 36, … Desenvolva
+#uma App em Python que gere esta série até o N-ésimo
+#termo. Este N-ésimo termo é digitado pelo usuário.
+
+"""
+n = int(input("Digite o N-ésimo termo: "))
+cont = 1
+i = 0
+
+while cont <= n:
+    i = cont * cont
+    print(i)
+    cont += 1
+"""
+
+
+
+#17) O valor aproximado do número p pode ser calculado
+#usando-se a série: S = 1 - 1/33 + 1/53 - 1/73 + 1/93 - …
+#Sendo PI = (S.32)**1/3. Desenvolva uma App em Python
+#que calcule e imprima o valor de p usando os 51 primeiros
+#termos da séria.
+
+"""
+n = 51
+S = 0
+x = 1
+
+for i in range(1, n+1):
+    if i == 1:
+        S += 1
+    if i % 2 == 0:
+        S -= (1/x**3)
+    if i % 3 == 0:
+        S += (1/x**3)
+    x += 2
+    PI = (S*32)**(1/3)
+print(PI)
+"""
